094738a2c3dc6b12e501006423e0ca4d85d30f13
shshrukh <shshrukh.arbaz@gmail.com>
2025-05-06 13:02:26 +0500

    functions and their types

---
+++ JsFunctions-07/logic.js
@@ -0,0 +1,29 @@
+// fucntion are reusable code block of code.
+// Types of functions
+//1. function declaratiopn
+
+function greet (){
+    return `Hello`
+}
+console.log(greet());
+
+
+
+//2. Function Expression 
+
+const hello =  function greetings (){
+    return `hello`
+}
+
+// In function expression we cant use function before
+
+//3. Arrow function
+
+const lol = ()=>{
+    return 2 + 4
+}
+console.log(lol());
+
+
+
+
