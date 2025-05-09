5de794d45c09949fa8c23bbf0219a5ddad631d68
shshrukh <shshrukh.arbaz@gmail.com>
2025-05-09 22:54:16 +0500

    using the concept of closure make ATM machine

--- JsClosure-09/logic.js
+++ JsClosure-09/logic.js
@@ -7,5 +7,56 @@ function outerFunction (){
     innerFunction()
     console.log(number);
 }
-outerFunction();
-console.log(number);
+// outerFunction();
+// console.log(number);
+
+
+// closure play an important role in progaraming in simply we say it cant forget the values if the function 
+
+
+//Project to make ATM machine
+
+function createAtm( ){
+    const userDetails={
+        password : 1122,
+        userAmount : 500,
+        passAttemps : 0
+    }
+    
+    return {
+        checkingBlance : function(pin ){
+            if (userDetails.passAttemps >= 3 ) return 'Account locked !';
+            if (pin === userDetails.password ) {
+                return `Your current amount is ${userDetails.userAmount}`
+            }else{
+                userDetails.passAttemps++;
+                return 'Wrong Password'
+            }
+        },
+        withdraw: function(pin, amount){
+            if(userDetails.passAttemps >= 3) return 'Account locked !';
+            if(amount > userDetails.userAmount ) return "Insufficient funds";
+            if( pin === userDetails.password){
+                return `you withdraw a amount:${amount} and your avalible balance is ${userDetails.userAmount -= amount}`
+
+            }else {
+                userDetails.passAttemps ++
+                return 'incorrect password'
+            }
+        }
+    }
+    
+}
+
+const atm = createAtm()
+console.log(atm.checkingBlance(1122));
+console.log(atm.withdraw(1122,300));
+console.log(atm.withdraw(1122,50));
+console.log(atm.withdraw(112,50));
+console.log(atm.withdraw(112,50));
+console.log(atm.withdraw(112,50));
+console.log(atm.withdraw(112,50));
+
+
+
+
