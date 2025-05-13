72b9f898bd71b188d920687b44a11fe0a3ec9cea
shshrukh <shshrukh.arbaz@gmail.com>
2025-05-13 10:48:49 +0500

    changes

--- JsErrorHandling-10/logic.js
+++ JsErrorHandling-10/logic.js
@@ -22,3 +22,45 @@ try {
     console.log("this is final block"); // this block is runs always if we got error either not.
     
 }
+
+
+
+
+
+
+
+
+
+// function one(){
+//     let a=0;
+//     console.log('hello', a)
+//     function two (){
+
+//     a +=1;
+//     console.log('b' , a);
+    
+// }
+// return two
+// }
+
+// function two (){
+
+//     a +=1;
+//     console.log('b' , a);
+    
+// }
+
+// const abc = one();
+// abc();
+
+// function countdown(n) {
+//   if (n === 0) {
+//     console.log("Boom!");
+//     return;
+//   }
+
+//   console.log("Countdown at:", n);
+//   console.log("Finished countdown for:", n);
+// }
+
+// countdown(3);
