1f0e664a3de5ee56d7e8c9a55c8e925e1df5a2c4
shshrukh <shshrukh.arbaz@gmail.com>
2025-05-19 23:41:10 +0500

    some detail touch about call, apply and bind methods

--- JsBindCallApply-013/logic.js
+++ JsBindCallApply-013/logic.js
@@ -34,18 +34,30 @@ console.log(res2);
 // now what if i have another object and same method of greet by making hard code simply i use call method 
 // and access those method in other objects 
 
+
 const userTwo = {name:"arbaz"};
 
 const res3 = userOne.greet.call(userTwo,"hello");
 console.log(res3);
+// Note : the value of this is depend upon how the function is call not where the funtion is define;
 
 
+// ---------apply()--------//
+// apply method is same as call but the arfuments are in array form
+
+const res4 = userOne.greet.apply(userTwo,['hello']);
+console.log(res4);
 
 
+//----------- bind() ----------//
+// bind return a new function with bound this ..
 
 
+const res5 = greetings.bind(user,"hello");
+console.log(res5); // res5 is here a function now 
 
+console.log(res5());
 
-
-//2.
-//3.
+// Doesn’t call the function immediately
+// Returns a new function with this permanently bound
+//Can be reused/called later
