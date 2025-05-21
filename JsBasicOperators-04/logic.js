5cefd1358a4813029efcb4d769c657b4592b2fef
shshrukh <shshrukh.arbaz@gmail.com>
2025-05-21 10:42:20 +0500

    Some coercion concepts how Js deal other datatype with Arathematic operators except addition sign

--- JsBasicOperators-04/logic.js
+++ JsBasicOperators-04/logic.js
@@ -65,6 +65,51 @@ console.log(null === undefined);  // false. === operator check both value and ty
 console.log(NaN === NaN); // false   why false? weird one,  Rule. NaN is not equla to anything, even iteslf
 
 
+// some more details about coercion 
+
+const num1 = 6;
+const str1 = '4';
+const str2 = 'abc';
+const str3 = 'efg'
+const num2 = 6;
+const result1 = num1 - str1; // what is the output? so the output is 2 and data type is number;
+const result2 = str1 - str2; // output is NaN. NaN datatype is number
+const result3 = num1 - str1; // output is NaN
+
+// why in result1 outPut is 2 how  num1 data type is number and string datatype is string but 4
+// coercion --> str1 the operator between - then num1 data type is number and str1 is  string 
+// Js try to solve this and its itself change the data type useing method str1.Number() and give result 6-4 = 2
+
+
+// secondt case str1 - str2 --> NaN 
+
+const a = '7';
+const b = 4;
+const res8 = a - b;
+const res9 = b - a;
+console.log(res8, 'this is res8', typeof res8);
+console.log(res9, 'this is res9', typeof res9);
+const userNmae = 'ali';
+const res10 = userNmae - b;
+const res4 = b - userNmae;
+const res5 = a - userNmae;
+console.log(res3, 'this is res3', typeof res10);
+console.log(res4, 'this is res4', typeof res4);
+console.log(res5, 'this is res5', typeof res5);
+console.log(NaN == NaN);
+console.log(typeof NaN);
+const d = [];
+const res6 = b - d;
+const res7 = d - b;
+
+console.log(res6, 'this is res6', typeof res6);
+console.log(res7, 'this is res7', typeof res7);
+console.log(d == false);
+console.log(d == 0);
+
+console.log(typeof []);
+console.log(typeof ['q', 'w'])
+console.log(null == undefined);
 
 
 
